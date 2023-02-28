2173daf58a1a56eeed64f6ca9e51e4443c7e44ed
prakash <prakash.maloo@iorbit-tech.com>
2023-02-28 13:41:14 +0530

    removed-subdivisions

--- ui/web/Sub-Division/src/Components/CreateForm/WithRouter.js
+++
@@ -1,9 +0,0 @@
-import { useNavigate, useParams } from "react-router-dom";
-import React from 'react'
-export function withRouter( Child ) {
-    return ( props ) => {
-      const params = useParams();
-      const navigate = useNavigate();
-      return <Child { ...props } params ={ params } navigate={navigate} />;
-    }
-  }
